923271ce5695aa2a2979cea9e1d0914c2aac71a4
Maksym <112901851+Maksym82@users.noreply.github.com>
2025-11-15 09:44:32 +0200

    Update styles slider

--- js/main.js
+++ js/main.js
@@ -2,5 +2,18 @@ const menuBtn = document.querySelector(".menu__btn");
 const menu = document.querySelector(".menu__list");
 
 menuBtn.addEventListener("click", () => {
-    menu.classList.toggle('active');
+  menu.classList.toggle("active");
+});
+
+const swiper = new Swiper(".projects__slider", {
+  // Optional parameters
+  loop: true,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+
 });
